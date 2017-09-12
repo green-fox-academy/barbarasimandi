1f71cc492121caae2f85c4afadee6f4eeae1ec42
unknown <barbarasimandi@gmail.com>
2017-09-12 14:28:47 +0200

    DrawTriangle

---
+++ week-02/day-1/src/DrawTriangle.java
@@ -0,0 +1,20 @@
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+
+        int num = 3;
+        String row = "";
+        for (int i = 0; i < num; i++) {
+            row = row + "*";
+            System.out.println(row);
+        }
+    }
+}
