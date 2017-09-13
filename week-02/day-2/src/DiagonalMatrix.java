afaa43ca69efabec47c74666276df8a93c41f113
unknown <barbarasimandi@gmail.com>
2017-09-13 11:06:29 +0200

    DiagonalMatrix

--- week-02/day-2/src/DiagonalMatrix.java
+++ week-02/day-2/src/DiagonalMatrix.java
@@ -1,3 +1,5 @@
+import javax.xml.bind.SchemaOutputResolver;
+
 public class DiagonalMatrix {
     // - Create (dynamically) a two dimensional array
     //   with the following matrix. Use a loop!
@@ -11,24 +13,31 @@ public class DiagonalMatrix {
 
     public static void main(String[] args) {
 
-        // We are going to have 4 rows and 4 columns.
+                    // We are going to have 4 rows and 4 columns.
 
-        int row = 4;
-        int col = 4;
+                    int size = 4;
 
-        int[][] multiArray = new int[row][col];
 
-        // Filling up the rows
-        for (int i = 0; i <= row; i++) {
-            //Filling up the columns
-            for (int j = 0; j <= col; j++) {
-                // If they're at the sam place, we should print 1, otherwise 0.
-                if (i == j) {
-                    int multiArray[i][j] = 1;
-                } else {
-                    int multiArray[i][j] = 0;
-                } System.out.println(multiArray[i][j]);
-            }
-        }
+                    int[][] multiArray = new int[size][size];
+
+                    // Filling up the rows
+                    for (int i = 0; i < size; i++) {
+                        //Filling up the columns
+                        for (int j = 0; j < size; j++) {
+
+                            /*multiArray[i][j] = 8; */
+
+                            // If they're at the sam place, we should print 1, otherwise 0.
+                            if (j == i) {
+                                multiArray[i][j] = 1;
+                            } else {
+                                multiArray[i][j] = 0;
+                            }
+                            // Printing the elements
+                            System.out.print(multiArray[i][j] + " ");
+                        }
+                        // Putting them in a new row.
+                        System.out.println();
+                    }
     }
 }
