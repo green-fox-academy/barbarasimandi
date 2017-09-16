cc2a5bf6edaa78549780b96baafaf83178d629d5
unknown <barbarasimandi@gmail.com>
2017-09-16 12:25:22 +0200

    DrawDiamond - still needs polishing

---
+++ week-02/day-1/src/DrawDiamond.java
@@ -0,0 +1,53 @@
+public class DrawDiamond {
+
+  public static void main(String[] args) {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // diamond like this:
+    //
+    //
+    //    *
+    //   ***
+    //  *****
+    // *******
+    //  *****
+    //   ***
+    //    *
+    //
+    // The diamond should have as many lines as the number was
+
+    double numberOfLines = 7;
+    double num = numberOfLines / 2;
+    double spaceIndex = num;
+    double starIndex = -1;
+
+    for (int i = 0; i < num; i++) {
+      spaceIndex = num - i;
+      starIndex += 2;
+
+      for (int j = 0; j < spaceIndex; j++) {
+        System.out.print(" ");
+      }
+
+      for (int k = 0; k < starIndex; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    for (int i = 0; i < num; i++) {
+      spaceIndex += 1;
+      starIndex -= 2;
+
+      for (int j = 0; j < spaceIndex; j++) {
+        System.out.print(" ");
+      }
+
+      for (int k = 0; k < starIndex; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
