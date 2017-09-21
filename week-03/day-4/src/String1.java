8792b58becead1bd6841c6db623dba8e490585c8
unknown <barbarasimandi@gmail.com>
2017-09-21 17:18:21 +0200

    String1

---
+++ week-03/day-4/src/String1.java
@@ -0,0 +1,18 @@
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+public class String1 {
+
+  public static void main(String[] args) {
+    System.out.println(string1("abcdxxxbb"));
+  }
+
+  public static String string1(String input) {
+
+    if (input.length() == 0) {
+      return "";
+    }
+    else {
+      return string1(input.substring(0, input.length() - 1)) + input.substring(input.length() - 1).replace("x", "y");
+    }
+  }
+}
