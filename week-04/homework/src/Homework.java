d81f8028861f93d012f127ac16f6747694eafb92
unknown <barbarasimandi@gmail.com>
2017-09-23 13:20:07 +0200

    Trees

---
+++ week-04/homework/src/Homework.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Homework {
+
+  public static void main(String[] args) {
+    HashMap<String, Object> birch = new HashMap<>();
+    birch.put("type", "birch");
+    birch.put("leaf color", "light green");
+    birch.put("age", 14);
+    birch.put("sex", "male");
+
+    HashMap<String, Object> gingkoBiloba = new HashMap<>();
+    gingkoBiloba.put("type", "gingko biloba");
+    gingkoBiloba.put("leaf color", "emerald");
+    gingkoBiloba.put("age", 42);
+    gingkoBiloba.put("sex", "female");
+
+    HashMap<String, Object> baobab = new HashMap<>();
+    baobab.put("type", "baobab");
+    baobab.put("leaf color", "khaki");
+    baobab.put("age", 60);
+    baobab.put("sex", "female");
+
+    HashMap<String, Object> eucalyptus = new HashMap<>();
+    eucalyptus.put("type", "eucalyptus");
+    eucalyptus.put("leaf color", "light green");
+    eucalyptus.put("age", 16);
+    eucalyptus.put("sex", "male");
+
+    HashMap<String, Object> oak = new HashMap<>();
+    oak.put("type", "oak");
+    oak.put("leaf color", "green");
+    oak.put("age", 5);
+    oak.put("sex", "male");
+
+  }
+}
