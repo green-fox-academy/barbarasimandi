ff4e2a737a1ffb5cb22dd2e68ea2e6a80279d61d
unknown <barbarasimandi@gmail.com>
2017-10-13 22:28:37 +0200

    BlackJack excercise done

--- week-05/lastExam/src/Card.java
+++ week-05/lastExam/src/Card.java
@@ -9,13 +9,13 @@ public class Card {
 
 
   public String getRandomValue() {
-    int random = (int) (Math.random() * 12);
+    int random = (int) (Math.random() * 13);
     String value = new String [] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}[random];
     return value;
   }
 
   @Override
   public String toString() {
-    return "" + value + " " + color + ", ";
+    return value + " " + color;
   }
 }
--- week-05/lastExam/src/Deck.java
+++ week-05/lastExam/src/Deck.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
   ArrayList<Card> listOfCards;
@@ -30,71 +31,77 @@ public class Deck {
       fillDeck();
       int howManyMore = number - 4;
       for (int i = 0; i < howManyMore; i++) {
-        listOfCards.add(getCardByColor(number).get(i));
+        listOfCards.add(getCardListByColor(number).get(i));
       }
     }
   }
 
   public void fillDeck() {
-    listOfCards.add(new Heart());
     listOfCards.add(new Club());
-    listOfCards.add(new Spade());
     listOfCards.add(new Diamond());
+    listOfCards.add(new Heart());
+    listOfCards.add(new Spade());
   }
 
-  public ArrayList<Card> getCardByColor(int num) {
+  public ArrayList<Card> getCardListByColor(int num) {
 
     getRandomColor();
 
     for (int i = 0; i < num; i++) {
 
-      if (getRandomColor().equals(DIAMOND)) {
+      if (getRandomColor().equals(CLUB)) {
+        listFromMoreThanFourCards.add(new Club());
+      }
+      else if (getRandomColor().equals(DIAMOND)) {
         listFromMoreThanFourCards.add(new Diamond());
-
-      } else if (getRandomColor().equals(SPADE)) {
+      }
+      else if (getRandomColor().equals(HEART)) {
+        listFromMoreThanFourCards.add(new Heart());
+      }
+      else if (getRandomColor().equals(SPADE)) {
         listFromMoreThanFourCards.add(new Spade());
 
-      } else if (getRandomColor().equals(HEART)) {
-        listFromMoreThanFourCards.add(new Heart());
-
-      } else if (getRandomColor().equals(CLUB)) {
-        listFromMoreThanFourCards.add(new Club());
       }
     } return listFromMoreThanFourCards;
   }
 
   public String getRandomColor() {
     int random = (int) (Math.random() * 4);
-    String color = new String [] {DIAMOND, SPADE, HEART, CLUB}[random];
+    String color = new String [] {CLUB, DIAMOND, HEART, SPADE}[random];
     return color;
   }
 
-
   public void shuffle() {
-
+    Collections.shuffle(listOfCards);
   }
 
-  private int getNumberOfCards(String type) {
-      int counter = 0;
-      for (int i = 0; i < listOfCards.size(); i++) {
-        if (listOfCards.get(i) instanceof Heart && type.equals(HEART)) {
-          counter++;
-        }
-        else if (listOfCards.get(i) instanceof Spade && type.equals(SPADE)) {
-          counter++;
-        }
-        else if (listOfCards.get(i) instanceof Diamond && type.equals(DIAMOND)) {
-          counter++;
-        }
-        else if (listOfCards.get(i) instanceof Club && type.equals(CLUB)) {
-          counter++;
-        }
-       }
+  public Card draw() {
+    Card tempCard = listOfCards.get(listOfCards.size() - 1);
+    listOfCards.remove(listOfCards.size() - 1);
+    return tempCard;
+  }
+
+  private int getTheNumberOfCardsInTheDeck(String type) {
+    int counter = 0;
+    for (int i = 0; i < listOfCards.size(); i++) {
+      if (listOfCards.get(i) instanceof Club && type.equals(CLUB)) {
+        counter++;
+      }
+      else if (listOfCards.get(i) instanceof Diamond && type.equals(DIAMOND)) {
+        counter++;
+      }
+      else if (listOfCards.get(i) instanceof Heart && type.equals(HEART)) {
+        counter++;
+      }
+      else if (listOfCards.get(i) instanceof Spade && type.equals(SPADE)) {
+        counter++;
+      }
+    }
       return counter;
     }
 
   @Override
   public String toString() {
-    return listOfCards.size() + " cards - " + getNumberOfCards(HEART) + " hearts " + getNumberOfCards(SPADE) + " spades " + getNumberOfCards(CLUB) + " clubs " + getNumberOfCards(DIAMOND) + " diamonds ";
+    return listOfCards.size() + " Cards - " + getTheNumberOfCardsInTheDeck(CLUB) + " Clubs " +  getTheNumberOfCardsInTheDeck(DIAMOND) + " Diamonds " + getTheNumberOfCardsInTheDeck(HEART) + " Hearts " + getTheNumberOfCardsInTheDeck(SPADE) + " Spades" ;
   }
 }
