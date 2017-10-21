10ac894266b36b26df88783cb6f41bfa88118629
unknown <barbarasimandi@gmail.com>
2017-10-21 23:31:22 +0200

    CreditCardReservation implementing 2 interfaces

--- week-06/day-2/src/creditcardreservation/main/java/CreditCardReservation.java
+++ week-06/day-2/src/creditcardreservation/main/java/CreditCardReservation.java
@@ -3,12 +3,14 @@ import creditcard.main.app.CreditCard;
 import creditcard.main.app.CreditCardy;
 import reservation.app.main.*;
 
-public class CreditCardReservation extends CreditCard implements Reservationy {
+public class CreditCardReservation implements CreditCardy, Reservationy {
 
   Reservation res;
+  CreditCard card;
 
   public CreditCardReservation() {
     res = new Reservation();
+    card = new CreditCard();
   }
 
   public String getDowBooking() {
@@ -19,7 +21,24 @@ public class CreditCardReservation extends CreditCard implements Reservationy {
     return res.getCodeBooking();
   }
 
+  public int getSumCVV() {
+    return card.getSumCVV();
+  }
+
+  public String getNameCardholder() {
+    return card.getNameCardholder();
+  }
+
+  public String getCodeAccount() {
+    return card.getCodeAccount();
+  }
+  
+  public int cumeSumCVV(String codeAccount) {
+    return card.cumeSumCVV(card.getCodeAccount());
+  }
+
   public String toString() {
-    return String.format("Booking#%s %s for %s Name=ABC%s CC#=%s CVV=%d %s", res.counter, getCodeBooking(), getDowBooking(), getNameCardholder(), getCodeAccount(), getSumCVV(), printWhetherValidated());
+    return String.format("Booking#%s %s for %s Name=ABC%s CC#=%s CVV=%d %s", res.counter, getCodeBooking(), getDowBooking(), getNameCardholder(), getCodeAccount(), getSumCVV(),
+        card.printWhetherValidated());
   }
 }
