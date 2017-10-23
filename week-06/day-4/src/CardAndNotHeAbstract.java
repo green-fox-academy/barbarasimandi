580995f2b1d7820ea0d1232ad71e9415af65b513
unknown <barbarasimandi@gmail.com>
2017-10-23 14:21:12 +0200

    BlackJack

--- week-06/day-4/src/CardAndNotHeAbstract.java
+++ week-06/day-4/src/CardAndNotHeAbstract.java
@@ -5,27 +5,34 @@ public class CardAndNotHeAbstract {
   public enum Rank {
     DEUCE(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
 
-    private int value;
+    public int value;
 
     Rank(int value) {
     this.value = value;
+    }
+    public int getValue() {
+      return value;
     }
   }
 
   public enum Color {BLACK, RED}
 
-  Suit suit;
-  Rank rank;
-  Color color;
+  private Suit suit;
 
+  private Rank rank;
+  private Color color;
   public CardAndNotHeAbstract(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
     color = getColor();
   }
 
-  public Color getColor() {
+  private Color getColor() {
     return (suit == Suit.CLUBS || suit == Suit.SPADES) ? Color.BLACK : Color.RED;
+  }
+
+  public int getValue() {
+    return rank.getValue();
   }
 
   @Override
--- week-06/day-4/src/Game.java
+++ week-06/day-4/src/Game.java
@@ -1,3 +1,43 @@
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+    static Deck d;
+    int opponentScore;
+    public enum Answer {yes, no}
+
+  public Game() {
+    d = new Deck();
+    opponentScore = (int)(15 + Math.random() * 7);
+  }
+
+  public static String getNextStep() {
+    System.out.println("Would you like another card?");
+    Scanner sc = new Scanner(System.in);
+    return sc.nextLine();
+  }
+
+  public static void main(String[] args) {
+    Game game = new Game();
+
+    int playerScore = 0;
+    int rounds = 0;
+
+    while (playerScore < 21 || Answer.valueOf(getNextStep()).equals(Answer.yes)) {
+      playerScore += d.pullRandom().getValue();
+      rounds++;
+      if (rounds > 2 || playerScore > 21 || Answer.valueOf(getNextStep()).equals(Answer.no)) {
+        break;
+      }
+    }
+
+      if (playerScore > 21) {
+        System.out.println("your scores are above 21. (" + playerScore + ") end of game" + "opponent's score: " + game.opponentScore);
+      }
+      else if (playerScore > game.opponentScore || playerScore == 21) {
+        System.out.println("you won. your score: " + playerScore + " opponent's score: " + game.opponentScore);
+      }
+      else {
+        System.out.println("opponent won. your score: " + playerScore + " opponent's score: " + game.opponentScore);
+      }
+  }
 }
