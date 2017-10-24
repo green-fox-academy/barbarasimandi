e75bb910664fccd354980b95b576f208f8d28f74
unknown <barbarasimandi@gmail.com>
2017-10-24 14:00:24 +0200

    Hello world REST App

---
+++ week-07/day-1/FirstSprigBootApp/src/main/java/com/greenfoxacademy/springstart/Application.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.springstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping(value="/hello")
+	@ResponseBody
+	public static String hello() {
+		return "Hello World!";
+	}
+}
