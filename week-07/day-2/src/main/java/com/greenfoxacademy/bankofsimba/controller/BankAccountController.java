113b30b902a83ba64df4fbd211d780eef06eb42b
unknown <barbarasimandi@gmail.com>
2017-10-25 14:25:24 +0200

    Filling with multiple accounts and showing their index

--- week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
+++ week-07/day-2/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -1,32 +1,46 @@
 package com.greenfoxacademy.bankofsimba.controller;
 
-
 import com.greenfoxacademy.bankofsimba.model.BankAccount;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BankAccountController {
 
-  BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
-  DecimalFormat df = new DecimalFormat("0.00##");
+  List<BankAccount> bank = new ArrayList<>();
+  BankAccount simbAccount = new BankAccount("Simba", 2000, "lion");
+  BankAccount mufasAccount = new BankAccount("Mufasa", 3000, "lion");
+  BankAccount pumbAccount = new BankAccount("Pumba", 500, "boar");
+  BankAccount timonAccount = new BankAccount("Timon", 400, "mongoose");
+  BankAccount rafikiAccount = new BankAccount("Rafiki", 10000, "monkey");
+  BankAccount zordonAccount = new BankAccount("Zordon", 0, "lion");
 
   @RequestMapping(value = "/bank")
   public String showAccount(Model model) {
-    model.addAttribute("name", String.format("%s", bankAccount.getName()));
-    model.addAttribute("balance", String.format("%.2f %s", bankAccount.getBalance(), "Zebra"));
-    model.addAttribute("animalType", String.format("%s",bankAccount.getAnimalType()));
-    return "default";
+    model.addAttribute("name", String.format("%s", simbAccount.getName()));
+    model.addAttribute("balance", String.format("%.2f %s", simbAccount.getBalance(), "Zebra"));
+    model.addAttribute("animalType", String.format("%s", simbAccount.getAnimalType()));
+    return "bank";
   }
 
   @RequestMapping(value = "/enjoy")
   public String htmlSubmit(Model model) {
     model.addAttribute("htmlText", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
     return "enjoy";
+  }
+
+  @RequestMapping(value = "/accounts")
+  public String fillCharacter(Model model) {
+  bank.add(simbAccount);
+  bank.add(mufasAccount);
+  bank.add(pumbAccount);
+  bank.add(timonAccount);
+  bank.add(rafikiAccount);
+  bank.add(zordonAccount);
+  model.addAttribute("bankAccounts", bank);
+    return "characters";
   }
 }
