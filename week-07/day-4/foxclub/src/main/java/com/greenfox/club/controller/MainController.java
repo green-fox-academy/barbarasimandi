3ab9fbe480a51298455c5449f6e9e68eba58dbd2
unknown <barbarasimandi@gmail.com>
2017-10-27 13:42:50 +0200

    Fox club so far (using model)

--- week-07/day-4/foxclub/src/main/java/com/greenfox/club/controller/MainController.java
+++ week-07/day-4/foxclub/src/main/java/com/greenfox/club/controller/MainController.java
@@ -3,6 +3,7 @@ package com.greenfox.club.controller;
 import com.greenfox.club.model.Fox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +13,8 @@ public class MainController {
   Fox fox;
 
   @RequestMapping(value = "/")
-  public String main() {
+  public String main(Model model) {
+    model.addAttribute("fox", fox);
     return "index";
   }
 }
--- week-07/day-4/foxclub/src/main/java/com/greenfox/club/model/Fox.java
+++ week-07/day-4/foxclub/src/main/java/com/greenfox/club/model/Fox.java
@@ -14,4 +14,36 @@ public class Fox {
   public Fox(String name) {
     this.name = name;
   }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setListOfTricks(List<Trick> listOfTricks) {
+    this.listOfTricks = listOfTricks;
+  }
+
+  public void setFood(Food food) {
+    this.food = food;
+  }
+
+  public void setDrink(Drink drink) {
+    this.drink = drink;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<Trick> getListOfTricks() {
+    return listOfTricks;
+  }
+
+  public Food getFood() {
+    return food;
+  }
+
+  public Drink getDrink() {
+    return drink;
+  }
 }
