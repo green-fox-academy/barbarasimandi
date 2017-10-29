3f335208e9b1ad4c6681b20f253a51bfff40f580
unknown <barbarasimandi@gmail.com>
2017-10-29 22:24:49 +0100

    renaming fixingBugs and messageServices excercise, removing useless things

--- week-07/day-3/fixingBugs/src/main/java/com/greenfox/error/controller/AppController.java
+++ week-07/day-3/fixingBugs/src/main/java/com/greenfox/error/controller/AppController.java
@@ -1,13 +1,14 @@
-package com.greenfox.error.model.controller;
+package com.greenfox.error.controller;
 
-import com.greenfox.error.model.config.UserService;
-import com.greenfox.error.model.model.User;
+import com.greenfox.error.config.UserService;
+import com.greenfox.error.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
@@ -15,16 +16,16 @@ public class AppController {
     @Autowired
     UserService service;
 
-    @GetMapping("/main")
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("new_user", new User());
         model.addAttribute("yolo", service.getAll());
         return "main";
     }
 
-    @PostMapping()
+    @PostMapping("/app")
     public String create(@ModelAttribute User user) {
         service.save(user);
-        return "redirect:/main";
+        return "redirect:/";
     }
 }
