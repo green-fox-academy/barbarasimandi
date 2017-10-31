1e1a2fef8f02b9a8e364ed70499bd90535e039b2
unknown <barbarasimandi@gmail.com>
2017-10-31 11:33:53 +0100

    Listing Todos from embedded database

---
+++ week-08/day-2/Listing_Todos/src/main/java/com/greenfox/todo/TodoApplication.java
@@ -0,0 +1,26 @@
+package com.greenfox.todo;
+
+import com.greenfox.todo.model.Todo;
+import com.greenfox.todo.repository.TodoRepository;
+import javax.sql.rowset.CachedRowSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication implements CommandLineRunner {
+
+	@Autowired
+	TodoRepository todoRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		todoRepository.save(new Todo("I have to learn Object Relational Mapping"));
+		todoRepository.save(new Todo("Feed the cat"));
+	}
+}
