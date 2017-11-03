7d479437b9fd28fc831fd744ee86145ffc2e4803
unknown <barbarasimandi@gmail.com>
2017-11-03 12:50:03 +0100

    Reddit started

--- week-08/day-5/src/main/java/com/greenfox/reddit/controller/PostController.java
+++ week-08/day-5/src/main/java/com/greenfox/reddit/controller/PostController.java
@@ -1,9 +1,11 @@
 package com.greenfox.reddit.controller;
 
+import com.greenfox.reddit.model.Post;
 import com.greenfox.reddit.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,10 +15,22 @@ public class PostController {
   @Autowired
   PostRepository postRepository;
 
-  @RequestMapping("/")
+  @GetMapping({"/", ""})
   public String listPosts(Model model) {
     model.addAttribute("posts", postRepository.findAll());
     return "posts";
   }
+
+  @GetMapping(value = "/add")
+  public String addPost(Model model) {
+    model.addAttribute("newPost", new Post());
+    return "addpost";
+  }
+
+//  @PostMapping(value = "/list/addTodo")
+//  public String addNew(@ModelAttribute Todo todo) {
+//    todoRepository.save(todo);
+//    return "redirect:/todo/list";
+//  }
 
 }
